4e42c24d23044991ac4043cedf595c9f4c534b8d
Glieb <gleblitvinenko3007@gmail.com>
2023-07-22 13:03:48 +0300

    Developed base serializers for all models in theatre app

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,1 +1,103 @@
+from django.db import transaction
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from theatre.models import (
+    Genre,
+    Actor,
+    Performance,
+    Play,
+    TheatreHall,
+    Ticket,
+    Reservation
+)
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name", "full_name")
+
+
+class TheatreHallSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TheatreHall
+        fields = ("id", "name", "seats_in_row", "capacity")
+
+
+class PlaySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Play
+        fields = (
+            "id",
+            "title",
+            "description",
+            "actors",
+            "genres"
+        )
+
+
+class PerformanceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Performance
+        fields = (
+            "id",
+            "show_time",
+            "play",
+            "theatre_hall"
+        )
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs=attrs)
+        Ticket.validate_ticket(
+            attrs["row"],
+            attrs["seat"],
+            attrs["theatre_hall"].theatre_hall,
+            ValidationError,
+        )
+        return data
+
+    class Meta:
+        model = Ticket
+        fields = (
+            "id",
+            "row",
+            "seat",
+            "performance"
+        )
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(
+        many=True,
+        read_only=False,
+        allow_empty=False
+    )
+
+    class Meta:
+        model = Reservation
+        fields = ("id", "tickets", "created_at")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            reservation = Reservation.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(
+                    reservation=reservation,
+                    **ticket_data
+                )
+            return reservation
