3f99a37fd31455c58b2842e630cb92387d2d464b
Glieb <gleblitvinenko3007@gmail.com>
2023-07-22 16:09:13 +0300

    Created urls for Actor and Genre ViewSet

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from theatre.views import (
+    ActorViewSet,
+    GenreViewSet
+)
+
+router = routers.DefaultRouter()
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
 
 
 urlpatterns = [
-
+    path("", include(router.urls)),
 ]
 
 app_name = "theatre"
