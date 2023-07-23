9d07be5a81f2dbe172245941eb4b30fe91465230
Glieb <gleblitvinenko3007@gmail.com>
2023-07-23 15:47:33 +0300

    Hid SECRET_KEY

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d-91jox66#y!bh*&j^ve3gb#4m%ei9bkw(pt_0#47ud4h!dp*8'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
